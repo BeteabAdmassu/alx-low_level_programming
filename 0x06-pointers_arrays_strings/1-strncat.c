b3229d15c6dc261623645c2eb21a0fe3fb81b219
Beteab Admassu <beteab.admassu.tadele@gmail.com>
2022-03-24 22:02:44 +0300

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,20 +9,20 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
-int j;
-i = 0;
-j = 0;
-while (dest[i] != '\0')
-{
-i++;
+        unsigned int i = 0;
+        int j = 0;
+
+        while (*(dest + i) != '\0')
+                i++;
+        while (*(src + j) != '\0')
+        {
+                if (j < n)
+                {
+                        *(dest + i) = *(src + j);
+                        i++;
+                }
+                j++;
+        }
+        return (dest);
 }
-while (j < n)
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
-return (dest);
-}
+
